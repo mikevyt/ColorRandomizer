cc7bd8fb045b376c032e6917306fe502dc96c929
Michael Vytlingam <michael.vytlingam@gmail.com>
2018-11-26 22:14:24 -0500

    Initial Commit

---
+++ ColorRandomizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorRandomizer
+//
+//  Created by Michael Vytlingam on 2018-11-26.
+//  Copyright © 2018 Michael Vytlingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
