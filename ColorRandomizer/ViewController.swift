8005550fbe59e647045080d386908b258d5c8f0b
Michael Vytlingam <michael.vytlingam@gmail.com>
2018-11-27 00:02:28 -0500

    adds random color generator and styling

--- ColorRandomizer/ViewController.swift
+++ ColorRandomizer/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomButton.layer.cornerRadius = 7
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func randomize(_ sender: UIButton) {
+        let r = CGFloat(Float.random(in: 0.0..<1.0))
+        let g = CGFloat(Float.random(in: 0.0..<1.0))
+        let b = CGFloat(Float.random(in: 0.0..<1.0))
+        let normalColor = UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
+        let compColor = getCompColor(color: normalColor)
+        background.backgroundColor = normalColor
+        randomButton.backgroundColor = compColor
+        print("Normal Color: ", normalColor)
+        print("Comp Color: ", compColor)
+        randomButton.setTitleColor(normalColor, for: [])
+    }
+    
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var randomButton: UIButton!
+    
+    func getCompColor(color: UIColor) -> UIColor {
+        let r = 1.0 - CIColor(color: color).red
+        let g = 1.0 - CIColor(color: color).green
+        let b = 1.0 - CIColor(color: color).blue
+        return UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
 }
 
