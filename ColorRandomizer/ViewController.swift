8d07083000d6d59b60a81e9b5bb5c2d5f5363586
Michael Vytlingam <michael.vytlingam@gmail.com>
2018-12-03 23:43:58 -0500

    uses hue values instead of rgb

--- ColorRandomizer/ViewController.swift
+++ ColorRandomizer/ViewController.swift
@@ -18,26 +18,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func randomize(_ sender: UIButton) {
-        let r = CGFloat(Float.random(in: 0.0..<1.0))
-        let g = CGFloat(Float.random(in: 0.0..<1.0))
-        let b = CGFloat(Float.random(in: 0.0..<1.0))
-        let normalColor = UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
-        let compColor = getCompColor(color: normalColor)
+        let h = CGFloat(Float.random(in: 0.0..<1.0))
+//        let s = CGFloat(Float.random(in: 0.0..<1.0))
+//        let v = CGFloat(Float.random(in: 0.0..<1.0))
+        let normalColor = UIColor.init(hue: h, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        let compColor = UIColor.init(hue: abs(h - 0.5), saturation: 1.0, brightness: 1.0, alpha: 1.0)
         background.backgroundColor = normalColor
         randomButton.backgroundColor = compColor
-        print("Normal Color: ", normalColor)
-        print("Comp Color: ", compColor)
         randomButton.setTitleColor(normalColor, for: [])
     }
     
     @IBOutlet weak var background: UIView!
     @IBOutlet weak var randomButton: UIButton!
-    
-    func getCompColor(color: UIColor) -> UIColor {
-        let r = 1.0 - CIColor(color: color).red
-        let g = 1.0 - CIColor(color: color).green
-        let b = 1.0 - CIColor(color: color).blue
-        return UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
-    }
 }
 
